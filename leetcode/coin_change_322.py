be0cfb292ce3328594c3e83b936c81b3012183d5
naceron <naceron@berkeley.edu>
2019-03-09 22:50:44 -0800

    First attempt iterative coinchange.py

---
+++ leetcode/coin_change_322.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        
+        for every amount
+        """
+        min_coins = [amount+1]*(amount+1)
+        min_coins[0] = 0
+        
+        for a in range(amount+1):
+            for coin in coins:
+                if coin <= a:
+                    min_coins[a] = min(min_coins[a], min_coins[a-coin] + 1)
+        
+        return -1 if min_coins[a] == amount+1 else min_coins[a]
